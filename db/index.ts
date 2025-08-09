68f14a6c13c6386e8c1fa4efea7710a0cd86ef86
Bhavin <stark.bhavin@gmail.com>
2025-08-09 13:41:52 +0530

    Changes link

--- db/index.ts
+++ db/index.ts
@@ -1,8 +1,9 @@
-import { Asset } from "expo-asset";
 import * as FileSystem from "expo-file-system";
 import * as SQLite from "expo-sqlite";
 
 const DBName = "sysscan_invoice_v2.db";
+const DB_URL =
+  "https://drive.google.com/file/d/11SnRf11_8T053xiyvvwgsN_1z6m5f9_T/view?usp=sharing";
 
 export async function openDatabase() {
   const dbDirectory = `${FileSystem.documentDirectory}SQLite`;
@@ -14,9 +15,10 @@ export async function openDatabase() {
 
   if (!(await FileSystem.getInfoAsync(dbPath)).exists) {
     console.log("Database file not found. Copying from assets...");
-    // eslint-disable-next-line @typescript-eslint/no-require-imports
-    const asset = Asset.fromModule(require(`../assets/${DBName}`));
-    await FileSystem.downloadAsync(asset.uri, dbPath);
+
+    // const asset = Asset.fromModule(require(`../assets/${DBName}`));
+    // await FileSystem.downloadAsync(asset.uri, dbPath);
+    await FileSystem.downloadAsync(DB_URL, dbPath);
     console.log("Database copied successfully.");
   } else {
     console.log("Database file already exists. Opening it.");
