5bd94451266c2649aebf2164347e6da56021dedb
Bhavin <stark.bhavin@gmail.com>
2025-08-14 18:06:43 +0530

    bug fixes

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,18 +1,16 @@
-import { Link, Stack, usePathname } from "expo-router";
+import { Link, Stack } from "expo-router";
 import { StyleSheet } from "react-native";
 
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 
 export default function NotFoundScreen() {
-  const pathName = usePathname();
-  console.log("NotFoundScreen", pathName);
   return (
     <>
       <Stack.Screen options={{ title: "Oops!" }} />
       <ThemedView style={styles.container}>
         <ThemedText type="title">This screen does not exist.</ThemedText>
-        <Link href="/home" style={styles.link}>
+        <Link href="/" style={styles.link}>
           <ThemedText type="link">Go to home screen!</ThemedText>
         </Link>
       </ThemedView>
